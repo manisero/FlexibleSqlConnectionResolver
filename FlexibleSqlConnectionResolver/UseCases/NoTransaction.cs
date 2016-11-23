1a170fb5aa1fd8b77631bc4381b025a2151244a3
manisero <michalaniserowicz@gmail.com>
2016-11-23 23:11:32 +0100

    Started defining NoTransaction use cases

--- FlexibleSqlConnectionResolver/UseCases/NoTransaction.cs
+++ FlexibleSqlConnectionResolver/UseCases/NoTransaction.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using FlexibleSqlConnectionResolver.ConnectionResolution;
 
 namespace FlexibleSqlConnectionResolver.UseCases
 {
@@ -11,15 +12,42 @@ namespace FlexibleSqlConnectionResolver.UseCases
 
         public override void Right()
         {
-            throw new NotImplementedException();
+            // Create connection per operation within task
+
+            using (var connectionResolver = new PerResolveSqlConnectionResolver(_connectionString))
+            {
+                ExecuteLongRunningTask(connectionResolver, "Task1");
+                ExecuteLongRunningTask(connectionResolver, "Task2");
+            }
         }
 
         public override void Wrong1()
         {
-            throw new NotImplementedException();
+            // Create single long-living connection per task
+
+            using (var connectionResolver = new SingletonSqlConnectionResolver(_connectionString))
+            {
+                ExecuteLongRunningTask(connectionResolver, "Task1");
+            }
+
+            using (var connectionResolver = new SingletonSqlConnectionResolver(_connectionString))
+            {
+                ExecuteLongRunningTask(connectionResolver, "Task2");
+            }
         }
 
         public override void Wrong2()
+        {
+            // Create single too-long-living connection
+
+            using (var connectionResolver = new SingletonSqlConnectionResolver(_connectionString))
+            {
+                ExecuteLongRunningTask(connectionResolver, "Task1");
+                ExecuteLongRunningTask(connectionResolver, "Task2");
+            }
+        }
+
+        private void ExecuteLongRunningTask(ISqlConnectionResolver connectionResolver, string taskName)
         {
             throw new NotImplementedException();
         }
